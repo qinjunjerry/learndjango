10bafd1efca1c3068fc3fa138f18396d3813e9ce
qinjunjerry <qinjunjerry@gmail.com>
2019-04-28 22:48:23 +0200

    fix bugs in was_published_recently()

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
+
 # Create your models here.
 
 class Question(models.Model):
@@ -13,7 +14,10 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        # return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
